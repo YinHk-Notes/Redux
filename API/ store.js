8db3a7827648f5013ce286a7cba76286b7af4792
Yin <67538811+YinHk@users.noreply.github.com>
2021-05-16 20:50:28 +0800

    Update and rename reference.js to  store.js

--- API/ store.js
+++ API/ store.js
@@ -3,3 +3,7 @@ import { createStore } from 'redux'
 //create store in ES5
 var createStore = require('redux').createStore
 
+*********************************************************************
+createStore(reducer, [preloadedState], [enhancer])
+
+//
